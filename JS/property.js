e1d670744258fb689919e3e9190bb9e65a3848b1
Anu Olusanya <ilorianuoluwapo@gmail.com>
2023-06-27 13:07:15 +0100

    created the property list page

---
+++ JS/property.js
@@ -0,0 +1,32 @@
+const properties =[
+     {
+        id: 1,
+        name: '',
+        category: 'Apartment',
+        status: 'For Sale',
+        size: '500 SqFt',
+        bedrooms: 3,
+        bathrooms: 3,
+        Garages: 1,
+        Location: 'Abuja',
+        year: 2020,
+        ImageSrc: "../assets/home1.jpg",
+        Agent: "",
+        date: "23-07-2022"
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
